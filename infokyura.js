9fb8b4930eee4205eb50221cc782b36b396205d2
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-04 16:20:42 +0700

    Update infokyura.js

--- infokyura.js
+++ infokyura.js
@@ -1,22 +1,22 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *Informasi Owner* ´ˎ˗
-│ ✎ Nama : Kyura 
-│    Kelas : 11 DKV 2,SMK N 1 SAWAN
-│    Umur : 16
-│    Asal : Bali 
+│ ✎ Nama : Tio 
+│    Kelas : lulus
+│    Umur : 19
+│    Asal : Tasikmalaya
 │    Status : gatau mw isi apa anj
 │    Suka : Jepang, Coding (dikit), B.Inggris
 │ ✎ _Official Grup_ :
-│    bit.ly/officialgrupkyurabot
+│    
 │ ✎ _Instagram_ :
-│    bit.ly/instagramkyura
+│   
 │ ✎ _YouTube_ : 
-│    bit.ly/Youtubekyura
+│    
 │ ✎ _FaceBook_ :
-│    bit.ly/facebookkyura
+│    
 │ ✎ _WhatsApp_ :
-│    bit.ly/whatsappkyura
+│    
 ╰‿‿‿‿‿‿‿‿
 `.trim(), m)
 }
